1b23c85a4e833c383ddbbe45bb373761e696270a
thulka2 <47118848+thulka2@users.noreply.github.com>
2020-06-24 18:24:44 -0500

    Updates

---
+++ precache-manifest.01057334cdfe7038038b985b62c52573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a66e586effdd3b4498d8216a12bd35",
+    "url": "/bob-test/index.html"
+  },
+  {
+    "revision": "731cea52ddb32b2beb2c",
+    "url": "/bob-test/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "2709fa6a7cd1efcde1e4",
+    "url": "/bob-test/static/css/main.2fa2f70a.chunk.css"
+  },
+  {
+    "revision": "731cea52ddb32b2beb2c",
+    "url": "/bob-test/static/js/2.239280a7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/bob-test/static/js/2.239280a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2709fa6a7cd1efcde1e4",
+    "url": "/bob-test/static/js/main.f9206780.chunk.js"
+  },
+  {
+    "revision": "46e52c3657fda1cf5461",
+    "url": "/bob-test/static/js/runtime-main.53e04bee.js"
+  }
+]);
